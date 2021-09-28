df84fbb9a54581fbd749a4caebbd1648009a6c51
Salah <S7baijan@hotmail.com>
2021-09-28 20:51:00 +0300

    add events constants

--- src/hooks/useTrackEvents.ts
+++ src/hooks/useTrackEvents.ts
@@ -1,16 +1,40 @@
 import { useUserQuery } from "../queries/graphql"
 import { amplitude } from "../utils/amplitude"
 
-
 export const useTrackEvent = () => {
+    console.log('xxxxxxxx')
     const { data, loading, error } = useUserQuery()
-
-    amplitude.track({
-        user_id: data?.user?.id,
-        user_properties: {
-            ...data?.user
+    if (data?.user) {
+        amplitude.track({
+            user_id: data?.user?.id,
+            user_properties: {
+                ...data?.user,
+            },
+            event_type: "HHHEY",
+        })
+    }
+    amplitude.track([
+        {
+            event_type: "some value", // required
+            user_id: "some id", // only required if device id is not passed in
+            device_id: "some id", // only required if user id is not passed in
+            event_properties: {
+                //...
+            },
+            user_properties: {
+                //...
+            },
         },
-        event_type:'HHHEY',
-    })
-    
+        {
+            event_type: "another value", // required
+            user_id: "some id", // only required if device id is not passed in
+            device_id: "some id", // only required if user id is not passed in
+            event_properties: {
+                //...
+            },
+            user_properties: {
+                //...
+            },
+        },
+    ])
 }
