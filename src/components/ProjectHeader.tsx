2858208a1e3b52c92afe81c098e4b472cf5f7a07
salah <s7baijan@hotmail.com>
2021-09-30 17:00:28 +0300

    add backgroundless feedbackness logo

--- src/components/ProjectHeader.tsx
+++ src/components/ProjectHeader.tsx
@@ -1,14 +1,15 @@
 import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 import {
-  Box, Flex,
+  Box,
+  Flex,
   HStack,
   IconButton,
-  Link, Select,
-  Stack,
-  useColorModeValue,
-  useDisclosure
+  Link,
+  Select,
+  Stack, useDisclosure
 } from "@chakra-ui/react";
 import React, { ReactNode } from "react";
+import FeedbacknessLogo from "../svgs/Feedbackness-logo.jpg";
 
 interface ProjectHeaderProps {}
 
@@ -21,7 +22,7 @@ const NavLink = ({ children }: { children: ReactNode }) => (
     rounded={"md"}
     _hover={{
       textDecoration: "none",
-      bg: useColorModeValue("gray.200", "gray.700"),
+      bg: "gray.200",
     }}
     href={"#"}
   >
@@ -42,7 +43,9 @@ export const ProjectHeader: React.FC<ProjectHeaderProps> = ({}) => {
           justifyContent={["space-between"]}
         >
           <HStack spacing={8} alignItems={"center"}>
-            <Box>Logo</Box>
+            <Box>
+              <img width={120} src={FeedbacknessLogo} loading="lazy" />
+            </Box>
             <Select placeholder="Select option">
               <option value="option1">Option 1</option>
               <option value="option2">Option 2</option>
@@ -63,16 +66,15 @@ export const ProjectHeader: React.FC<ProjectHeaderProps> = ({}) => {
           />
         </Flex>
         {isOpen ? (
-            <Box pb={4} display={{ md: "none" }}>
-              <Stack as={"nav"} spacing={4}>
-                {Links.map((link) => (
-                  <NavLink key={link}>{link}</NavLink>
-                ))}
-              </Stack>
-            </Box>
-          ) : null}
+          <Box pb={4} display={{ md: "none" }}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
-
       <Box p={4}>Main Content Here</Box>
     </>
   );
