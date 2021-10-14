5021435d5404f34f76b92e276d192121ba3049d0
salah <s7baijan@hotmail.com>
2021-10-14 13:20:21 +0300

    update store, adding feedback

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,5 +1,14 @@
 import create from "zustand";
-import { ALL_TIME, LAST_6_MONTHS, LAST_MONTH, LAST_WEEK } from "../constants";
+import {
+  ALL_FEEDBACK,
+  ALL_TIME,
+  ISSUE,
+  LAST_6_MONTHS,
+  LAST_MONTH,
+  LAST_WEEK,
+  OTHER,
+  IDEA
+} from "../constants";
 
 type ProjectTimeRange =
   | typeof ALL_TIME
@@ -7,15 +16,27 @@ type ProjectTimeRange =
   | typeof LAST_MONTH
   | typeof LAST_6_MONTHS;
 
+type ProjectFeedbackType =
+  | typeof ALL_FEEDBACK
+  | typeof ISSUE
+  | typeof IDEA
+  | typeof OTHER;
 
 export interface ProjectStore {
   projectTimeRange: ProjectTimeRange;
+  projectFeedbackType: ProjectFeedbackType
   changeProjectTimeRange: (p: ProjectTimeRange) => void;
+  changeProjectFeedbackType: (p: ProjectFeedbackType) => void;
+
 }
 
 export const useProjectStore = create<ProjectStore>((set) => ({
   projectTimeRange: ALL_TIME,
+  projectFeedbackType: ALL_FEEDBACK,
   changeProjectTimeRange: (projectTimeRange: ProjectTimeRange) => {
     set({ projectTimeRange });
+  },
+  changeProjectFeedbackType: (projectFeedbackType: ProjectFeedbackType) => {
+    set({ projectFeedbackType });
   },
 }));
