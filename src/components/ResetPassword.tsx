30a5985695613278c8fcee0838cfdf8751ab627d
salah <s7baijan@hotmail.com>
2021-10-20 00:46:32 +0300

    unite all forms with same svg

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -1,71 +1,75 @@
-import { Box, Text } from "@chakra-ui/layout"
-import { Flex } from "@chakra-ui/react"
-import React, { useState } from "react"
-import toast from "react-hot-toast"
-import { Link } from "react-router-dom"
-import { COLOR_MAIN_MEDIUM_DARK, ROUTES } from "../constants"
-import { useResetPasswordMutation } from "../queries/graphql"
-import { extractError } from "../utils/extractError"
-import { AuthForm } from "./AuthForm"
-import { AuthFormButton } from "./AuthFormButton"
-import { InputField } from "./InputField"
-import { useHistory, useParams } from 'react-router-dom'
-  
-const PersonYogaSvg = React.lazy(() => import('../svgs/PersonYogaSvg'))
+import { Box, Text } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+import { COLOR_MAIN_MEDIUM_DARK, ROUTES } from "../constants";
+import { useResetPasswordMutation } from "../queries/graphql";
+import { extractError } from "../utils/extractError";
+import { AuthForm } from "./AuthForm";
+import { AuthFormButton } from "./AuthFormButton";
+import { InputField } from "./InputField";
+import { useHistory, useParams } from "react-router-dom";
+import PersonWithPhoneSvg from "../svgs/PersonWithPhoneSvg";
+
+const PersonYogaSvg = React.lazy(() => import("../svgs/PersonYogaSvg"));
 interface ResetPasswordProps {}
 
 const ResetPassword: React.FC<ResetPasswordProps> = ({}) => {
-    const [email, setEmail] = useState("")
-    const [isSubmit, setIsSubmit] = useState(false)
-    const [resetPassword] = useResetPasswordMutation()
-    const { token } = useParams<{token : string}>()
- 
-    return (
-        <AuthForm
-            bgSvg={<PersonYogaSvg />}
-            bgColor={COLOR_MAIN_MEDIUM_DARK}
-            heading={"Reset Your Password"}
-            onSubmit={async (e) => {
-                try {
-                    setIsSubmit(true)
-                    await resetPassword({ variables: { email } })
-                    toast.success('Check your email to reset your password ')
-                    setIsSubmit(false)
-                } catch (e) {
-                    toast.error(extractError(e))
-                    setIsSubmit(false)
-                }
-                 
-            }}
-        >
-            <Box py={3}>
-                <InputField
-                    name="email"
-                    placeholder="Enter your email"
-                    label="Email address"
-                    type="email"
-                    value={email}
-                    onChange={(e) => {
-                        setEmail((e.target as HTMLInputElement).value)
-                        console.log(email)
-                    }}
-                />
-            </Box>
-            <Box py={3}>
-                <AuthFormButton type="submit" isSubmit={isSubmit}>
-                    <Text color="white">Reset Password</Text>
-                </AuthFormButton>
-            </Box>
-            <Box py={3}>
-                <Link to={ROUTES.SIGN_IN}>
-                    <Flex justifyContent="center">
-                        <Text>Found Your Password? </Text>
-                        <Text fontWeight="bold">&nbsp;Sign in</Text>
-                    </Flex>
-                </Link>
-            </Box>
-        </AuthForm>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [resetPassword] = useResetPasswordMutation();
+  const { token } = useParams<{ token: string }>();
 
-export default ResetPassword
+  return (
+    <AuthForm
+      bgSvg={
+        <Box mt={"-20"}>
+          <PersonWithPhoneSvg />
+        </Box>
+      }
+      bgColor={COLOR_MAIN_MEDIUM_DARK}
+      heading={"Reset Your Password"}
+      onSubmit={async (e) => {
+        try {
+          setIsSubmit(true);
+          await resetPassword({ variables: { email } });
+          toast.success("Check your email to reset your password ");
+          setIsSubmit(false);
+        } catch (e) {
+          toast.error(extractError(e));
+          setIsSubmit(false);
+        }
+      }}
+    >
+      <Box py={3}>
+        <InputField
+          name="email"
+          placeholder="Enter your email"
+          label="Email address"
+          type="email"
+          value={email}
+          onChange={(e) => {
+            setEmail((e.target as HTMLInputElement).value);
+            console.log(email);
+          }}
+        />
+      </Box>
+      <Box py={3}>
+        <AuthFormButton type="submit" isSubmit={isSubmit}>
+          <Text color="white">Reset Password</Text>
+        </AuthFormButton>
+      </Box>
+      <Box py={3}>
+        <Link to={ROUTES.SIGN_IN}>
+          <Flex justifyContent="center">
+            <Text>Found Your Password? </Text>
+            <Text fontWeight="bold">&nbsp;Sign in</Text>
+          </Flex>
+        </Link>
+      </Box>
+    </AuthForm>
+  );
+};
+
+export default ResetPassword;
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,74 +1,79 @@
-import { Box, Divider, HStack, Text } from "@chakra-ui/layout"
-import { Flex } from "@chakra-ui/react"
-import React, { useState } from "react"
-import { Link } from "react-router-dom"
-import { COLOR_MAIN_LIGHT, ROUTES } from "../constants"
-import { AuthForm } from "./AuthForm"
-import { AuthFormButton } from "./AuthFormButton"
-import { InputField } from "./InputField"
-import { SocialMediaButtons } from "./SocialMediaButtons"
-import { toast } from 'react-hot-toast'
+import { Box, Divider, HStack, Text } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { COLOR_MAIN_LIGHT, ROUTES } from "../constants";
+import { AuthForm } from "./AuthForm";
+import { AuthFormButton } from "./AuthFormButton";
+import { InputField } from "./InputField";
+import { SocialMediaButtons } from "./SocialMediaButtons";
+import { toast } from "react-hot-toast";
+import PersonWithPhoneSvg from "../svgs/PersonWithPhoneSvg";
 
-const PersonWalkingSvg = React.lazy(() => import("../svgs/PersonWalkingSvg"))
+const PersonWalkingSvg = React.lazy(() => import("../svgs/PersonWalkingSvg"));
 
 interface SignInProps {}
 const SignIn: React.FC<SignInProps> = ({}) => {
-    const [isSubmit, setIsSubmit] = useState(false)
-    return (
-        <AuthForm
-            heading={"Log in"}
-            bgSvg={<PersonWalkingSvg />}
-            bgColor={COLOR_MAIN_LIGHT}
-            onSubmit={(e) => {
-                setIsSubmit(true)
-                toast.error('s')
-            }}
-        >
-            <Box py={3}>
-                <InputField
-                    name="email"
-                    placeholder="Enter your email"
-                    label="Email address"
-                    type="email"
-                 />
-            </Box>
-            <Box py={3}>
-                <InputField
-                    name="password"
-                    placeholder="Enter your password"
-                    label="Password"
-                    type="Password"
-                 />
-            </Box>
-            <Box py={3}>
-                <AuthFormButton type="submit" isSubmit={isSubmit}>
-                    <Text color="white">Login</Text>
-                </AuthFormButton>
-            </Box>
-            <HStack>
-                <Divider />
-                <Text color="gray.500" fontWeight="bold">
-                    OR
-                </Text>
-                <Divider />
-            </HStack>
-            <Box py={3}>
-                <SocialMediaButtons />
-            </Box>
-            <Box py={3}>
-                <Link to={ROUTES.SIGN_UP}>
-                    <Flex justifyContent="center">
-                        <Text>Don't have an account? </Text>
-                        <Text fontWeight="bold">&nbsp;Create one</Text>
-                    </Flex>
-                </Link>
-            </Box>
-            <Box>
-                <Link to={ROUTES.RESET_PASSWORD}>
-                    <Text>Forgot Your Password? </Text>
-                </Link>
-            </Box>
-        </AuthForm>
-    )
-}
-export default SignIn
+  const [isSubmit, setIsSubmit] = useState(false);
+  return (
+    <AuthForm
+      heading={"Log in"}
+      bgSvg={
+        <Box mt={"-20"}>
+          <PersonWithPhoneSvg />
+        </Box>
+      }
+      bgColor={COLOR_MAIN_LIGHT}
+      onSubmit={(e) => {
+        setIsSubmit(true);
+        toast.error("s");
+      }}
+    >
+      <Box py={3}>
+        <InputField
+          name="email"
+          placeholder="Enter your email"
+          label="Email address"
+          type="email"
+        />
+      </Box>
+      <Box py={3}>
+        <InputField
+          name="password"
+          placeholder="Enter your password"
+          label="Password"
+          type="Password"
+        />
+      </Box>
+      <Box py={3}>
+        <AuthFormButton type="submit" isSubmit={isSubmit}>
+          <Text color="white">Login</Text>
+        </AuthFormButton>
+      </Box>
+      <HStack>
+        <Divider />
+        <Text color="gray.500" fontWeight="bold">
+          OR
+        </Text>
+        <Divider />
+      </HStack>
+      <Box py={3}>
+        <SocialMediaButtons />
+      </Box>
+      <Box py={3}>
+        <Link to={ROUTES.SIGN_UP}>
+          <Flex justifyContent="center">
+            <Text>Don't have an account? </Text>
+            <Text fontWeight="bold">&nbsp;Create one</Text>
+          </Flex>
+        </Link>
+      </Box>
+      <Box>
+        <Link to={ROUTES.RESET_PASSWORD}>
+          <Text>Forgot Your Password? </Text>
+        </Link>
+      </Box>
+    </AuthForm>
+  );
+};
+export default SignIn;
