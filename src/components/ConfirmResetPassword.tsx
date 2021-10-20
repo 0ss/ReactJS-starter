298a792e81da4081574253eb4830b58c9e400722
salah <s7baijan@hotmail.com>
2021-10-20 20:15:51 +0300

    add password and confirm password states

--- src/components/ConfirmResetPassword.tsx
+++ src/components/ConfirmResetPassword.tsx
@@ -3,7 +3,12 @@ import { Flex } from "@chakra-ui/react";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { Link, useParams } from "react-router-dom";
-import { COLOR_MAIN_DARK, COLOR_MAIN_LIGHT, COLOR_MAIN_MEDIUM_DARK, ROUTES } from "../constants";
+import {
+  COLOR_MAIN_DARK,
+  COLOR_MAIN_LIGHT,
+  COLOR_MAIN_MEDIUM_DARK,
+  ROUTES,
+} from "../constants";
 import { useResetPasswordMutation } from "../queries/graphql";
 import PersonWithPhoneSvg from "../svgs/PersonWithPhoneSvg";
 import { extractError } from "../utils/extractError";
@@ -14,18 +19,16 @@ import { InputField } from "./InputField";
 interface ConfirmResetPasswordProps {}
 
 const ConfirmResetPassword: React.FC<ConfirmResetPasswordProps> = ({}) => {
-    const [confirmResetPasswordInput, setConfirmResetPasswordInput] = useState({
-        name: "",
-        email: "",
-        password: "",
-    });
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const [isSubmit, setIsSubmit] = useState(false);
   const [resetPassword] = useResetPasswordMutation();
   const { token } = useParams<{ token: string }>();
-//   onChange={(e) => {
-//     setEmail((e.target as HTMLInputElement).value);
-//     console.log(email);
-//   }}
+  //   onChange={(e) => {
+  //     setEmail((e.target as HTMLInputElement).value);
+  //     console.log(email);
+  //   }}
   return (
     <AuthForm
       bgSvg={
@@ -34,47 +37,48 @@ const ConfirmResetPassword: React.FC<ConfirmResetPasswordProps> = ({}) => {
         </Box>
       }
       bgColor={COLOR_MAIN_DARK}
-      heading={"Set Your New Password"}
-    //   onSubmit={async (e) => {
-    //     try {
-    //       setIsSubmit(true);
-    //       await resetPassword({ variables: { email } });
-    //       toast.success("Check your email to reset your password ");
-    //       setIsSubmit(false);
-    //     } catch (e) {
-    //       toast.error(extractError(e));
-    //       setIsSubmit(false);
-    //     }
-    //   }}
+      heading={"Set Or Reset Your New Password"}
+      onSubmit={async (e) => {
+        try {
+          console.log("hey");
+
+          setIsSubmit(true);
+          if (confirmPassword !== password) {
+            toast.error("Passwords don't match ");
+          }
+          console.log("2222222222");
+
+          setIsSubmit(false);
+        } catch (e) {
+          toast.error(extractError(e));
+          setIsSubmit(false);
+        }
+      }}
     >
-      <Box py={3}>
-      <InputField
-          name="password"
-          placeholder="Enter your password"
-          label="New Password"
-          type="Password"
-        />
-      </Box>
       <Box py={3}>
         <InputField
           name="password"
           placeholder="Enter your password"
+          label="New Password"
+          type="Password"
+          onChange={(e) => setPassword((e.target as HTMLInputElement).value)}
+        />
+      </Box>
+      <Box py={3}>
+        <InputField
+          name="confirm-password"
+          placeholder="Enter your password"
           label="Confirm New Password"
           type="Password"
+          onChange={(e) =>
+            setConfirmPassword((e.target as HTMLInputElement).value)
+          }
         />
-        </Box>
+      </Box>
       <Box py={3}>
         <AuthFormButton type="submit" isSubmit={isSubmit}>
           <Text color="white">Reset Password</Text>
         </AuthFormButton>
-      </Box>
-      <Box py={3}>
-        <Link to={ROUTES.SIGN_IN}>
-          <Flex justifyContent="center">
-            <Text>Found Your Password? </Text>
-            <Text fontWeight="bold">&nbsp;Sign in</Text>
-          </Flex>
-        </Link>
       </Box>
     </AuthForm>
   );
