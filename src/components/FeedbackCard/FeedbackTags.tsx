3e4371266eacf5c4026ec80c4100b38c195e574f
salah <s7baijan@hotmail.com>
2021-10-21 13:52:21 +0300

    make feedback tag more customizble

--- src/components/FeedbackCard/FeedbackTags.tsx
+++ src/components/FeedbackCard/FeedbackTags.tsx
@@ -4,13 +4,24 @@ import { IDEA, ISSUE, OTHER } from "../../constants";
 
 interface FeedbackTagsProps {
   type: typeof ISSUE | typeof IDEA | typeof OTHER;
+  size?: string;
+  count?: number | string;
 }
 
-export const FeedbackTags: React.FC<FeedbackTagsProps> = ({ type }) => {
+export const FeedbackTags: React.FC<FeedbackTagsProps> = ({
+  type,
+  size,
+  count,
+}) => {
   const colors = {
     issue: "red",
     idea: "yellow",
     other: "gray",
   };
-  return <Badge fontSize={'sm'} colorScheme={colors[type]}>{type.toUpperCase()}</Badge>;
+  return (
+    <Badge fontSize={size} colorScheme={colors[type]}>
+      {type.toUpperCase()}
+      {count ? `:${{ count }}` : null}
+    </Badge>
+  );
 };
