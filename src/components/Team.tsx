05f140b73bb307a3634d4fac55a505cfa95417d8
salah <s7baijan@hotmail.com>
2021-10-31 13:30:23 +0300

    add team page template

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,14 +1,62 @@
-import React from 'react'
+import {
+    Box, Heading, VStack
+} from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { AiOutlineTeam } from "react-icons/ai";
+import { IoAnalyticsOutline, IoSettingsOutline } from "react-icons/io5";
+import { VscSignOut } from "react-icons/vsc";
+import { lazily } from "react-lazily";
+import { useParams } from "react-router";
+import {
+    ROUTES
+} from "../constants";
+import { useAuthToken } from "../hooks/useAuthToken";
+import { useProjectLocation } from "../hooks/useProjectLocation";
 
-interface TeamProps {
-    
-}
+const { ProjectSidebar } = lazily(() => import("./ProjectSidebar"));
+const { NavLink } = lazily(() => import("./NavLink"));
 
-export const Team : React.FC<TeamProps>= ({}) => {
-    console.log('dd')
-    return (
-        <div>
-            s
-        </div>
-    )
-}
+
+interface TeamProps {}
+export const Team: React.FC<TeamProps> = ({}) => {
+  const [authToken, setAuthToken] = useAuthToken();
+  const location = useProjectLocation();
+  const { id } = useParams<{id: string}>()
+  
+
+  return (
+    <Flex mx={{ base: "0", md: "14" }} maxWidth={"full"}>
+      <ProjectSidebar>
+        <VStack spacing={5} align={"stretch"}>
+          <NavLink
+            name={"Dashboard"}
+            url={`${location}/`}
+            icon={<IoAnalyticsOutline size={18} />}
+          />
+          <NavLink
+            name={"Team"}
+            url={`${location}/team`}
+            icon={<AiOutlineTeam size={18} />}
+          />
+          <NavLink
+            name={"Settings"}
+            url={`${location}/settings`}
+            icon={<IoSettingsOutline size={18} />}
+          />
+        </VStack>
+        <VStack mt={"48"} spacing={3} align={"stretch"}>
+          <NavLink
+            name={"Sign out"}
+            icon={<VscSignOut size={18} />}
+            url={ROUTES.HOME}
+            onClick={() => setAuthToken("")}
+          />
+        </VStack>
+      </ProjectSidebar>
+      <Box p={"14"} maxWidth={"full"} alignItems={"center"}>
+        <Heading mb={5}>Analytics Overview</Heading>
+      </Box>
+    </Flex>
+  );
+};
