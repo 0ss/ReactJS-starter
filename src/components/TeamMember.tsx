43074a26e2c744d16870badc9c62dab4a2cc9e88
salah <s7baijan@hotmail.com>
2021-11-01 23:28:06 +0300

    add team member content

---
+++ src/components/TeamMember.tsx
@@ -0,0 +1,16 @@
+import { Flex, Badge, Text } from "@chakra-ui/layout";
+import React from "react";
+
+interface TeamMemberProps {
+  email: string;
+  role: "member" | "admin";
+}
+
+export const TeamMember: React.FC<TeamMemberProps> = ({ email, role }) => {
+  return (
+    <Flex alignItems={"center"} justifyContent={"space-between"} my={"3"}>
+      <Text>{email}</Text>
+      <Badge colorScheme={"red"}>{role}</Badge>
+    </Flex>
+  );
+};
