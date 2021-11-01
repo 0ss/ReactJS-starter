f531dbbe001886905ce27686ce4c1a00401b7d9a
salah <s7baijan@hotmail.com>
2021-11-01 12:46:26 +0300

    add user team table template

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, Heading, HStack, Spacer, VStack } from "@chakra-ui/layout";
+import { Badge, Box, Divider, Heading, HStack, Spacer, VStack } from "@chakra-ui/layout";
 import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
 import { lazily } from "react-lazily";
@@ -20,7 +20,37 @@ export const Team: React.FC<TeamProps> = ({}) => {
       <Box p={"14"} width={"2xl"} alignItems={"center"}>
         <Heading mb={5}>Team</Heading>
         <Box borderRadius={"lg"} bg={"white"} alignItems={"start"} p={"5"}>
-            
+            <Flex
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              my={'3'}
+            >
+              <Text>Salah@salah.com</Text>
+              <Badge colorScheme={'red'}>admin</Badge>
+            </Flex>
+            <Divider />
+
+            <Flex
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              my={'3'}
+
+            >
+              <Text>Salah@salah.com</Text>
+              <Badge>member</Badge>
+            </Flex>
+            <Divider />
+            <Flex
+              alignItems={"center"}
+              justifyContent={"space-between"}
+              my={'3'}
+
+            >
+              <Text>Salah@salah.com</Text>
+              <Badge>member</Badge>
+            </Flex>
+            <Divider />
+
         </Box>
       </Box>
     </Flex>
