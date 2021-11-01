37147affffb3a6e3d02c6b4db3cf63ec23d34c74
salah <s7baijan@hotmail.com>
2021-11-01 12:41:48 +0300

    fix team width

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,14 +1,10 @@
-import { Box, Heading, VStack } from "@chakra-ui/layout";
-import { Flex } from "@chakra-ui/react";
+import { Badge, Box, Heading, HStack, Spacer, VStack } from "@chakra-ui/layout";
+import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
-import { AiOutlineTeam } from "react-icons/ai";
-import { IoAnalyticsOutline, IoSettingsOutline } from "react-icons/io5";
-import { VscSignOut } from "react-icons/vsc";
 import { lazily } from "react-lazily";
 import { useParams } from "react-router";
-import { ROUTES } from "../constants";
-import { useAuthToken } from "../hooks/useAuthToken";
 import { useProjectLocation } from "../hooks/useProjectLocation";
+import { InputField } from "./InputField";
 
 const { ProjectSidebar } = lazily(() => import("./ProjectSidebar"));
 const { NavLink } = lazily(() => import("./NavLink"));
@@ -21,8 +17,11 @@ export const Team: React.FC<TeamProps> = ({}) => {
   return (
     <Flex mx={{ base: "0", md: "14" }} maxWidth={"full"}>
       <ProjectSidebar projectLocation={location} />
-      <Box p={"14"} maxWidth={"full"} alignItems={"center"}>
-        <Heading mb={5}>Analytics Overview</Heading>
+      <Box p={"14"} width={"2xl"} alignItems={"center"}>
+        <Heading mb={5}>Team</Heading>
+        <Box borderRadius={"lg"} bg={"white"} alignItems={"start"} p={"5"}>
+            
+        </Box>
       </Box>
     </Flex>
   );
